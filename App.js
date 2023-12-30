79195d959bdd0594f7b43aa2408d8ae8946ff730
Rajkumar Bhatt <mr.bhatt2348@gmail.com>
2023-12-30 10:56:16 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const rootFromHTML = document.getElementById("root");
-const heading = React.createElement('div', { id: 'parent' },
-    [React.createElement('div', { className: 'child' },
-        [React.createElement('h1', {}, "Hello React from Nested statements..."), React.createElement('h2', {}, "Hello React from Nested statements...")]),
-    React.createElement('div', { className: 'child' }, [React.createElement('h1', {}, "Hello React from Nested statements..."),
-    React.createElement('h2', {}, "Hello React from Nested statements...")])])
-const root = ReactDOM.createRoot(rootFromHTML);
-root.render(heading);
+const heading = React.createElement("h1", {}, "Raj is here...");
+const jsxHeading = <h1>Coming directly from JSX...</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(jsxHeading);
